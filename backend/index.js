a7367eadf798daf25b2ddb36a2c59dba3f2c84a9
ShaYri12 <sharjeelb60@gmail.com>
2024-02-29 02:25:31 +0500

    ver

--- backend/index.js
+++ backend/index.js
@@ -12,22 +12,11 @@ import cartRoute from './routes/cart.js'
 
 dotenv.config();
 const app = express();
-const allowedOrigins = [
-    "https://fast-food-gamma.vercel.app",
-    "https://fast-food-ljab.vercel.app",
-    "http://localhost:5173",
-  ];
-  app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-    })
-  );
+const port = process.env.PORT ||  8000
+const corsOption = {
+    origin: true,
+    credentials: true,
+};
 
 //testing
 app.get('/',(req, res)=>{
@@ -47,6 +36,7 @@ const connect = async () => {
 
 //middleware
 app.use(express.json())
+app.use(cors(corsOption))
 app.use(cookieParser())
 
 //Routes
